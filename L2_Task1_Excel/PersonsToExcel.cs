cf92fe2e151da76c5d4020712ba834df65bf2397
m.ilnitsky <m.ilnitsky@academ-it.ru>
2019-06-28 18:20:28 +0700

    L2_Task1_Excel: исправления замечаний №1.2

--- L2_Task1_Excel/PersonsToExcel.cs
+++ L2_Task1_Excel/PersonsToExcel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 
 namespace L2_Task1_Excel
 {
@@ -6,18 +7,24 @@ namespace L2_Task1_Excel
     {
         private static void Main()
         {
-            var persons = new Person[7];
-
-            persons = new Person[]{new Person("Иванов", "Иван", "333-111", 19),
-            new Person("Петров", "Пётр", "333-222", 23),
-            new Person("Николаев", "Николай", "333-333", 29),
-            new Person("Васильев", "Василий", "333-444", 31),
-            new Person("Владимиров", "Владимир", "333-555", 37),
-            new Person("Дмитриев", "Дмитрий", "333-666", 41),
-            new Person("Альмухамедова", "Эльза", "737-737", 17)};
-
-            using (var workbook = new PersonsWorkbook(persons))
+            var persons = new[]
             {
+                new Person("Иванов", "Иван", "333-111", 19),
+                new Person("Петров", "Пётр", "333-222", 23),
+                new Person("Николаев", "Николай", "333-333", 29),
+                new Person("Васильев", "Василий", "333-444", 31),
+                new Person("Владимиров", "Владимир", "333-555", 37),
+                new Person("Дмитриев", "Дмитрий", "333-666", 41),
+                new Person("Альмухамедова", "Эльза", "737-737", 17)
+            };
+
+            using (var workbook = new PersonsWorkbook(persons, 2, 2))
+            {
+                workbook.SetDefaultStyle(Color.Black);
+
+                workbook.SetProperties("Список людей", "Список контактов", "Михаил Ильницкий", "Academ-It-School.ru", "Первый xlsx");
+                workbook.SetPageLayoutView(true);
+
                 workbook.SaveToFile("..\\..\\persons.xlsx");
             }
 
--- L2_Task1_Excel/PersonsWorkbook.cs
+++ L2_Task1_Excel/PersonsWorkbook.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using OfficeOpenXml;
+using OfficeOpenXml.Style;
 
 namespace L2_Task1_Excel
 {
@@ -10,30 +12,155 @@ namespace L2_Task1_Excel
         private readonly ExcelWorkbook _workbook;
         private readonly ExcelWorksheet _worksheet;
 
+        private readonly int _leftColumn;
+        private readonly int _rightColumn;
+        private readonly int _headerRow;
+        private readonly int _topBodyRow;
+        private readonly int _bottomBodyRow;
+
         private bool _disposed;
 
-        public PersonsWorkbook(Person[] persons)
+        public PersonsWorkbook(Person[] persons) : this(persons, 1, 1) { }
+
+        public PersonsWorkbook(Person[] persons, int leftCell, int topCell)
         {
+            _leftColumn = leftCell;
+            _rightColumn = _leftColumn + 4;
+            _headerRow = topCell;
+            _topBodyRow = topCell + 1;
+            _bottomBodyRow = topCell + persons.Length;
+
             _package = new ExcelPackage();
             _workbook = _package.Workbook;
             _worksheet = _package.Workbook.Worksheets.Add("Persons");
 
-            _worksheet.Cells[1, 1].Value = "ID";
-            _worksheet.Cells[1, 2].Value = "Family";
-            _worksheet.Cells[1, 3].Value = "Name";
-            _worksheet.Cells[1, 4].Value = "Phone";
-            _worksheet.Cells[1, 5].Value = "Age";
+            _worksheet.Cells[_headerRow, leftCell].Value = "ID";
+            _worksheet.Cells[_headerRow, leftCell + 1].Value = "Family";
+            _worksheet.Cells[_headerRow, leftCell + 2].Value = "Name";
+            _worksheet.Cells[_headerRow, leftCell + 3].Value = "Phone";
+            _worksheet.Cells[_headerRow, leftCell + 4].Value = "Age";
 
             for (var i = 0; i < persons.Length; ++i)
             {
-                var row = i + 2;
+                var row = _topBodyRow + i;
 
-                _worksheet.Cells[row, 1].Value = i;
-                _worksheet.Cells[row, 2].Value = persons[i].Family;
-                _worksheet.Cells[row, 3].Value = persons[i].Name;
-                _worksheet.Cells[row, 4].Value = persons[i].Phone;
-                _worksheet.Cells[row, 5].Value = persons[i].Age;
+                _worksheet.Cells[row, leftCell].Value = i;
+                _worksheet.Cells[row, leftCell + 1].Value = persons[i].Family;
+                _worksheet.Cells[row, leftCell + 2].Value = persons[i].Name;
+                _worksheet.Cells[row, leftCell + 3].Value = persons[i].Phone;
+                _worksheet.Cells[row, leftCell + 4].Value = persons[i].Age;
             }
+
+            _worksheet.Cells.AutoFitColumns(0);
+        }
+
+        public void SetProperties(string name, string title, string author, string company, string comments)
+        {
+            _worksheet.Name = name;
+
+            _worksheet.HeaderFooter.OddHeader.CenteredText = "&24&U&\"Arial,Regular Bold\"" + title;
+            _worksheet.HeaderFooter.OddFooter.RightAlignedText =
+                string.Format("Страница {0} из {1}", ExcelHeaderFooter.PageNumber, ExcelHeaderFooter.NumberOfPages);
+            _worksheet.HeaderFooter.OddFooter.CenteredText = ExcelHeaderFooter.SheetName;
+
+            _worksheet.Workbook.Properties.Title = title;
+            _worksheet.Workbook.Properties.Author = author;
+            _worksheet.Workbook.Properties.Comments = comments;
+            _worksheet.Workbook.Properties.Company = company;
+        }
+
+        public void SetDefaultStyle(Color color)
+        {
+            SetBodyStyle(Color.White, Color.LightGray, color);
+            SetHeaderStyle(color, Color.White);
+        }
+
+        public void SetHeaderStyle(Color backgroundColor, Color color)
+        {
+            using (var range = _worksheet.Cells[_headerRow, _leftColumn, _headerRow, _rightColumn])
+            {
+                range.Style.Font.Bold = true;
+                range.Style.Font.Color.SetColor(color);
+                range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                range.Style.Fill.BackgroundColor.SetColor(backgroundColor);
+                range.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            }
+
+            using (var range = _worksheet.Cells[_headerRow, _leftColumn, _headerRow, _rightColumn - 1])
+            {
+                range.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                range.Style.Border.Right.Color.SetColor(color);
+            }
+        }
+
+        public void SetBodyStyle(Color backgroundColor1, Color backgroundColor2, Color color)
+        {
+            using (var range = _worksheet.Cells[_topBodyRow, _leftColumn, _bottomBodyRow, _rightColumn])
+            {
+                range.Style.Font.Bold = false;
+                range.Style.Font.Color.SetColor(color);
+                range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                range.Style.Fill.BackgroundColor.SetColor(backgroundColor1);
+                range.Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                range.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                range.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                range.Style.Border.Bottom.Color.SetColor(color);
+                range.Style.Border.Right.Color.SetColor(color);
+            }
+
+            for (var i = _topBodyRow; i <= _bottomBodyRow; ++i)
+            {
+                if ((i - _topBodyRow) % 2 != 0)
+                {
+                    using (var range = _worksheet.Cells[i, _leftColumn, i, _rightColumn])
+                    {
+                        range.Style.Fill.BackgroundColor.SetColor(backgroundColor2);
+                    }
+                }
+            }
+
+            using (var range = _worksheet.Cells[_topBodyRow, _leftColumn, _bottomBodyRow, _leftColumn])
+            {
+                range.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                range.Style.Border.Left.Color.SetColor(color);
+            }
+
+            using (var range = _worksheet.Cells[_topBodyRow, _leftColumn + 1, _bottomBodyRow, _rightColumn - 1])
+            {
+                range.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+            }
+        }
+
+        public void SetTableBorder(ExcelBorderStyle borderStyle, Color color)
+        {
+            using (var range = _worksheet.Cells[_headerRow, _leftColumn, _headerRow, _rightColumn])
+            {
+                range.Style.Border.Top.Style = borderStyle;
+                range.Style.Border.Top.Color.SetColor(color);
+            }
+
+            using (var range = _worksheet.Cells[_bottomBodyRow, _leftColumn, _bottomBodyRow, _rightColumn])
+            {
+                range.Style.Border.Bottom.Style = borderStyle;
+                range.Style.Border.Bottom.Color.SetColor(color);
+            }
+
+            using (var range = _worksheet.Cells[_headerRow, _leftColumn, _bottomBodyRow, _leftColumn])
+            {
+                range.Style.Border.Left.Style = borderStyle;
+                range.Style.Border.Left.Color.SetColor(color);
+            }
+
+            using (var range = _worksheet.Cells[_headerRow, _rightColumn, _bottomBodyRow, _rightColumn])
+            {
+                range.Style.Border.Right.Style = borderStyle;
+                range.Style.Border.Right.Color.SetColor(color);
+            }
+        }
+
+        public void SetPageLayoutView(bool pageLayoutView)
+        {
+            _worksheet.View.PageLayoutView = pageLayoutView;
         }
 
         ~PersonsWorkbook()
