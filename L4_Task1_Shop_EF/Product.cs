8b9161a1b79206a18d0df91efae85397d32868ab
m.ilnitsky <m.ilnitsky@academ-it.ru>
2019-07-15 20:53:01 +0700

    L4_Task1_Shop_EF: ...

--- L4_Task1_Shop_EF/Product.cs
+++ L4_Task1_Shop_EF/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,8 @@ namespace L4_Task1_Shop_EF
         public string Name { get; set; }
 
         [Required]
+        public decimal Price { get; set; }
+
         public int[] CategoryIds { get; set; }
 
         public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
@@ -24,7 +27,7 @@ namespace L4_Task1_Shop_EF
         {
             var stringBuilder = new StringBuilder();
 
-            stringBuilder.AppendFormat("Id:{0,3} Name: {1, 10} Categories:[", Id, Name);
+            stringBuilder.AppendFormat("Id:{0,3} Name: {1, 10}  Price: {2, 8}  Categories:[", Id, Name, Price);
 
             if (Categories.Count > 0)
             {
--- L4_Task1_Shop_EF/ProgramShopEF.cs
+++ L4_Task1_Shop_EF/ProgramShopEF.cs
@@ -15,9 +15,19 @@ namespace L4_Task1_Shop_EF
                 bookShop.AddCategories(new List<string> { "книга", "журнал", "газета", "блокнот", "календарь", "карандаши", "ручки", "фломастеры" });
                 bookShop.PrintCategories();
 
+                bookShop.AddProducts(new List<Product>
+                {
+                    new Product{Name = "Газетный журнал", Price=100, Categories = new List<Category>{new Category{Name = "газета"}, new Category{Name="журнал"}}},
+                    new Product{Name = "Журнальная газетёнка", Price = 150, Categories = new List<Category>{new Category{Name = "газета"}, new Category{Name="журнал"}}},
+                    new Product{Name = "Питер Таймс", Price = 250, Categories = new List<Category>{new Category{Name = "газета"}}},
+                    new Product{Name = "Питер Пост", Price = 250, Categories = new List<Category>{new Category{Name = "журнал"}}},
+                    new Product{Name = "Корея сегодня", Price = 550, Categories = new List<Category>{new Category{Name = "газета"}, new Category{Name="журнал"}}}
+                });
+                bookShop.PrintProducts();
+
                 bookShop.AddCustomers(new List<Customer>
                 {
-                    new Customer{Name = "Валя", Surname = "Попов", Phone = "777-22-33", Mail = "popa@popov.net"},
+                    new Customer{Name = "Валя", Surname = "Попов", Phone = "777-22-33", Mail = "valy@popov.net"},
                     new Customer{Name = "Коля", Surname = "Кольянов", Phone = "777-22-11", Mail = "kol@koliyanov.net"},
                     new Customer{Name = "Толя", Surname = "Тольянов", Phone = "777-22-22", Mail = "tolyan@miru.net"},
                     new Customer{Name = "Поля", Surname = "Польянова", Phone = "666-66-66", Mail = "polya@vsegda.da"},
