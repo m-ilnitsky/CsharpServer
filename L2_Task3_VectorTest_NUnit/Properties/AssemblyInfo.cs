8503a4404205e095cda85eef1d3c4bbf3b7c0bd0
Mike <ilnitsky@ngs.ru>
2019-12-02 00:42:00 +0700

    L2_Task3_VectorTest_NUnit: реализовано тестирование вектора с помощью NUnit

---
+++ L2_Task3_VectorTest_NUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("L2_Task3_VectorTest_NUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("L2_Task3_VectorTest_NUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bba68161-5414-497a-bc37-d02722a80035")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ L2_Task3_VectorTest_NUnit/UnitTestOfVectorGetScalarProduct.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using NUnit.Framework;
+
+using L2_Task3_Vector;
+
+namespace L2_Task3_VectorTest_NUnit
+{
+    [TestFixture]
+    public class UnitTestOfVectorGetScalarProduct
+    {
+        private static bool Equals(double value1, double value2)
+        {
+            return Math.Abs(value1 - value2) <= 1e-10;
+        }
+
+        [Test]
+        public void TestGetScalarProductForVectorsWithLength1()
+        {
+            var vector1 = new Vector(new double[] { 1 });
+            var vector2 = new Vector(new double[] { 3 });
+
+            var trueResult = 3d;
+            var currentResult = Vector.GetScalarProduct(vector1, vector2);
+
+            Assert.IsTrue(Equals(trueResult, currentResult), "Результат скалярного произведения (" + currentResult + ") не равен эталону (" + trueResult + ")");
+        }
+
+        [Test]
+        public void TestGetScalarProductForVectorsWithLength2()
+        {
+            var vector1 = new Vector(new double[] { 1, 3 });
+            var vector2 = new Vector(new double[] { 2, 4 });
+
+            var trueResult = 14d;
+            var currentResult = Vector.GetScalarProduct(vector1, vector2);
+
+            Assert.IsTrue(Equals(trueResult, currentResult), "Результат скалярного произведения (" + currentResult + ") не равен эталону (" + trueResult + ")");
+        }
+
+        [Test]
+        public void TestGetScalarProductForVectorsWithLength3()
+        {
+            var vector1 = new Vector(new double[] { 1, 3, 5 });
+            var vector2 = new Vector(new double[] { 2, 4, 6 });
+
+            var trueResult = 44d;
+            var currentResult = Vector.GetScalarProduct(vector1, vector2);
+
+            Assert.IsTrue(Equals(trueResult, currentResult), "Результат скалярного произведения (" + currentResult + ") не равен эталону (" + trueResult + ")");
+        }
+
+        [Test]
+        public void TestGetScalarProductWhereFirstVectorIsLonger()
+        {
+            var vector1 = new Vector(new double[] { 1, 3, 5 });
+            var vector2 = new Vector(new double[] { 2, 4 });
+
+            var trueResult = 14d;
+            var currentResult = Vector.GetScalarProduct(vector1, vector2);
+
+            Assert.IsTrue(Equals(trueResult, currentResult), "Результат скалярного произведения (" + currentResult + ") не равен эталону (" + trueResult + ")");
+        }
+
+        [Test]
+        public void TestGetScalarProductWhereSecondVectorIsLonger()
+        {
+            var vector1 = new Vector(new double[] { 1, 3 });
+            var vector2 = new Vector(new double[] { 2, 4, 6 });
+
+            var trueResult = 14d;
+            var currentResult = Vector.GetScalarProduct(vector1, vector2);
+
+            Assert.IsTrue(Equals(trueResult, currentResult), "Результат скалярного произведения (" + currentResult + ") не равен эталону (" + trueResult + ")");
+        }
+    }
+}
